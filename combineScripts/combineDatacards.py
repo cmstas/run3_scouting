00c2f45f149b4927c36d85dd1adff7ede52b1fa9
fernance <celia.fernandez.madrazo@cern.ch>
2025-01-16 11:39:54 -0800

    Add systematics merging in combineScripts/combineDatacards.py

--- combineScripts/combineDatacards.py
+++ combineScripts/combineDatacards.py
@@ -16,6 +16,12 @@ outDir = ("%s/datacards_all_"%(thisDir))+today+"_allEras"
 if not os.path.exists(outDir):
     os.makedirs(outDir)
 
+# Systematics to combine
+systematics = []
+systematics.append(['CMS_eff_sel','CMS_eff_sel_2022','CMS_eff_sel_2023'])
+systematics.append(['CMS_eff_trg','CMS_eff_trg_2022','CMS_eff_trg_2023'])
+systematics.append(['mcstat_ch','stat22_ch','stat23_ch'])
+
 # Loop in cards of dir 1, look for the equivalent in dir 2 and merge:
 for card1 in os.listdir(inDir1):
     if '.txt' not in card1 or 'combine' not in card1:
@@ -30,8 +36,20 @@ for card1 in os.listdir(inDir1):
         for portion in card2.split('_')[:-1]:
             card_name2 += (portion + '_')
         if card_name1==card_name2:
+            tomerge_card1 = 'tomerge_' + card1
+            tomerge_card2 = 'tomerge_' + card2
+            os.system('cp %s/%s %s/%s'%(inDir1, card1, inDir1, tomerge_card1))
+            os.system('cp %s/%s %s/%s'%(inDir2, card2, inDir2, tomerge_card2))
+            # Correlate systematics
+            os.system("sed -i 's/CMS_eff_sel_2022/CMS_eff_sel     /g' %s/%s"%(inDir1, card1))
+            os.system("sed -i 's/CMS_eff_sel_2023/CMS_eff_sel     /g' %s/%s"%(inDir2, card2))
+            os.system("sed -i 's/CMS_eff_trg_2022/CMS_eff_trg     /g' %s/%s"%(inDir1, card1))
+            os.system("sed -i 's/CMS_eff_trg_2023/CMS_eff_trg     /g' %s/%s"%(inDir2, card2))
+            # Uncorrelate systematics
+            os.system("sed -i 's/mcstat_ch/stat22_ch/g' %s/%s"%(inDir1, card1))
+            os.system("sed -i 's/mcstat_ch/stat23_ch/g' %s/%s"%(inDir2, card2))
             print("COMBINING ", card_name1)
             print("combineCards.py -S %s/%s %s/%s > %s/%sallEras.txt "%(inDir1, card1, inDir2, card2, outDir, card_name1))
-            os.system("combineCards.py -S %s/%s %s/%s > %s/%sallEras.txt "%(inDir1, card1, inDir2, card2, outDir, card_name1))
+            os.system("combineCards.py -S %s/%s %s/%s > %s/%sallEras.txt "%(inDir1, tomerge_card1, inDir2, tomerge_card2, outDir, card_name1))
             os.system("text2workspace.py %s/%sallEras.txt"%(outDir, card_name1))
             break
