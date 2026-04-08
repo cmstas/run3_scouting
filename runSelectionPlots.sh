349c207be0385fa4339394991d69d8da3586171d
fernance <celia.fernandez.madrazo@cern.ch>
2026-04-08 08:59:25 -0700

    bash file to run selection plots

---
+++ runSelectionPlots.sh
@@ -0,0 +1,50 @@
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022postEE --noMuonHitSel --lxySel 0.0 0.2 --outSuffix noHit_0p0To0p2
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022 --noMuonHitSel       --lxySel 0.0 0.2 --outSuffix noHit_0p0To0p2
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022postEE --noMuonHitSel --lxySel 0.2 1.0 --outSuffix noHit_0p2To1p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022 --noMuonHitSel       --lxySel 0.2 1.0 --outSuffix noHit_0p2To1p0
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022postEE --noMuonHitSel --lxySel 1.0 2.4 --outSuffix noHit_1p0To2p4
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022 --noMuonHitSel       --lxySel 1.0 2.4 --outSuffix noHit_1p0To2p4
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022postEE --noMuonHitSel --lxySel 2.4 3.1 --outSuffix noHit_2p4To3p1
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022 --noMuonHitSel       --lxySel 2.4 3.1 --outSuffix noHit_2p4To3p1
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022postEE --noMuonHitSel --lxySel 3.1 7.0 --outSuffix noHit_3p1To7p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022 --noMuonHitSel       --lxySel 3.1 7.0 --outSuffix noHit_3p1To7p0
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022postEE --noMuonHitSel --lxySel 7.0 11.0 --outSuffix noHit_7p0To11p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022 --noMuonHitSel       --lxySel 7.0 11.0 --outSuffix noHit_7p0To11p0
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022postEE --noMuonHitSel --lxySel 11.0 16.0 --outSuffix noHit_11p0To16p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022 --noMuonHitSel       --lxySel 11.0 16.0 --outSuffix noHit_11p0To16p0
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022postEE --noMuonHitSel --lxySel 16.0 70.0 --outSuffix noHit_16p0To70p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioB1_Mpi-5_MA-2p40_ctau-10mm_2022 --noMuonHitSel       --lxySel 16.0 70.0 --outSuffix noHit_16p0To70p0
+#
+#
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022postEE --noMuonHitSel --lxySel 0.0 0.2 --outSuffix noHit_0p0To0p2
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022 --noMuonHitSel       --lxySel 0.0 0.2 --outSuffix noHit_0p0To0p2
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022postEE --noMuonHitSel --lxySel 0.2 1.0 --outSuffix noHit_0p2To1p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022 --noMuonHitSel       --lxySel 0.2 1.0 --outSuffix noHit_0p2To1p0
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022postEE --noMuonHitSel --lxySel 1.0 2.4 --outSuffix noHit_1p0To2p4
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022 --noMuonHitSel       --lxySel 1.0 2.4 --outSuffix noHit_1p0To2p4
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022postEE --noMuonHitSel --lxySel 2.4 3.1 --outSuffix noHit_2p4To3p1
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022 --noMuonHitSel       --lxySel 2.4 3.1 --outSuffix noHit_2p4To3p1
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022postEE --noMuonHitSel --lxySel 3.1 7.0 --outSuffix noHit_3p1To7p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022 --noMuonHitSel       --lxySel 3.1 7.0 --outSuffix noHit_3p1To7p0
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022postEE --noMuonHitSel --lxySel 7.0 11.0 --outSuffix noHit_7p0To11p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022 --noMuonHitSel       --lxySel 7.0 11.0 --outSuffix noHit_7p0To11p0
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022postEE --noMuonHitSel --lxySel 11.0 16.0 --outSuffix noHit_11p0To16p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022 --noMuonHitSel       --lxySel 11.0 16.0 --outSuffix noHit_11p0To16p0
+#
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022postEE --noMuonHitSel --lxySel 16.0 70.0 --outSuffix noHit_16p0To70p0
+python3 fillHistosScouting.py --inDir /ceph/cms/store/user/fernance/Run3ScoutingOutput/looperOutput_Feb-07-2024_2022_wrapped --inSample Signal_ScenarioA_Mpi-4_MA-1p33_ctau-10mm_2022 --noMuonHitSel       --lxySel 16.0 70.0 --outSuffix noHit_16p0To70p0
